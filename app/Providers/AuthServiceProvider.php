2aa82c507915c6d84afda8aed7271f8e448999c8
jeff <sleet.ww@gmail.com>
2022-03-31 05:24:28 +0800

    debug

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
+use Laravel\Passport\RouteRegistrar;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,7 +27,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes(null, ['middleware' => 'passport.guards']);
+        Passport::routes(function (RouteRegistrar $router) {
+            $router->forAccessTokens();
+        }, ['middleware' => 'passport.guards']);
 
     }
 }
