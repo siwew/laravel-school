157ddec809e37e3ab2815e9d9c50832b4ca31b55
jeff <sleet.ww@gmail.com>
2022-03-31 12:53:09 +0800

    debug

--- app/Model/Teacher.php
+++ app/Model/Teacher.php
@@ -5,11 +5,12 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Auth\Authenticatable;
 
 class Teacher extends Model
 {
 
-    use Notifiable, HasApiTokens;
+    use Notifiable, HasApiTokens, Authenticatable;
 
 
     protected $fillable = [
